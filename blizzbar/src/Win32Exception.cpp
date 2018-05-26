3e4b7561f5a7230e5cb321f3fcd6f7c5395a42d5
Austin Wagner <austinwagner@gmail.com>
2018-05-26 19:48:00 -0400

    Take error code as parameter to Win32Exception

--- blizzbar/src/Win32Exception.cpp
+++ blizzbar/src/Win32Exception.cpp
@@ -5,48 +5,56 @@
 #include <locale>
 #include <codecvt>
 
-Win32Exception::Win32Exception() 
-{ 
-	initMessage(""); 
-}
-
-Win32Exception::Win32Exception(const std::string& detail) 
-{ 
-	initMessage(detail); 
-}
-
 namespace
 {
     struct LocalFreeDeleter
     {
         void operator()(void* p) const { if (p != nullptr) LocalFree(p); }
     };
+
+    std::string FormatSystemMessage(uint32_t errorCode, const std::string& detail)
+    {
+	    std::unique_ptr<wchar_t, LocalFreeDeleter> errorMessageBuffer;
+	    const auto len = FormatMessageW(
+		    FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		    nullptr,
+		    errorCode,
+		    MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		    reinterpret_cast<LPWSTR>(&errorMessageBuffer),
+		    0,
+		    nullptr);
+
+	    const std::wstring errorMessageWide(errorMessageBuffer.get(), len);
+
+        // Since exceptions deriving from std::exception have to be able to return
+        // a char*, we convert to UTF-8 to be sure that nothing is lost in translation
+	    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+	    const auto errorMessage = converter.to_bytes(errorMessageWide);
+
+	    return detail.empty()
+		    ? errorMessage
+		    : (detail + " " + errorMessage);
+    }
 }
 
-void Win32Exception::initMessage(const std::string& detail)
+Win32Exception::Win32Exception()
+    : Win32Exception(GetLastError())
 {
-	const auto errorCode = GetLastError();
+}
 
-	std::unique_ptr<wchar_t, LocalFreeDeleter> errorMessageBuffer;
-	const auto len = FormatMessageW(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		nullptr,
-		errorCode,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		reinterpret_cast<LPWSTR>(&errorMessageBuffer),
-		0,
-		nullptr);
+Win32Exception::Win32Exception(const std::string& detail)
+    : Win32Exception(GetLastError(), detail)
+{
+}
 
-	const std::wstring errorMessageWide(errorMessageBuffer.get(), len);
+Win32Exception::Win32Exception(uint32_t error)
+    : Win32Exception(error, "")
+{
+}
 
-    // Since exceptions deriving from std::exception have to be able to return
-    // a char*, we convert to UTF-8 to be sure that nothing is lost in translation
-	std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-	const auto errorMessage = converter.to_bytes(errorMessageWide);
-
-	m_message = detail.empty()
-		? errorMessage
-		: (detail + " " + errorMessage);
+Win32Exception::Win32Exception(uint32_t error, const std::string& detail)
+    : m_message{FormatSystemMessage(error, detail)}
+{
 }
 
 const char* Win32Exception::what() const
--- blizzbar/src/Win32Exception.h
+++ blizzbar/src/Win32Exception.h
@@ -1,16 +1,17 @@
 #pragma once
 #include <exception>
 #include <string>
+#include <cstdint>
 
 class Win32Exception : public std::exception
 {
 public:
 	Win32Exception();
+    explicit Win32Exception(uint32_t error);
 	explicit Win32Exception(const std::string& detail);
+    Win32Exception(uint32_t error, const std::string& detail);
 	const char* what() const override;
 
 private:
-	void initMessage(const std::string& detail);
-
 	std::string m_message;
 };
